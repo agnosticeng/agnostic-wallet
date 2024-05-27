f663082f294f49b00e3d88b29a0092e911908e97
Yann Amsellem <yann@agnostic.engineering>
2024-05-27 15:40:41 +0200

    chore: bootstrap

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import "$lib/styles/index.css";
+
+  import SkipLink from "$lib/components/nav/SkipLink.svelte";
+</script>
+
+<SkipLink href="#main" />
+
+<main id="main">
+  <slot />
+</main>
+
+<style>
+  main {
+    position: relative;
+    margin: 0 auto;
+    overflow: hidden;
+    overflow-y: auto;
+    height: 100%;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+  <title>Wallet</title>
+</svelte:head>
+
+<h1>Welcome to The Agnostic Wallet</h1>
